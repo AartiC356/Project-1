b6c321b24520d1f8f08e78818aed4bdfc694d79c
AartiC356 <122652285+AartiC356@users.noreply.github.com>
2023-03-09 22:03:41 -0800

    1.Added `clearInterval()` method to maintain 10 sec intervals. 2. `setInterval()` method place changed.  3. Added comments and fixed layout
    Adding the clearInterval method has helped in fixing the blinking effect the app was creating.
    setInterval is not a part of another function now and is working smoothly.
    Adding comments and fixing the layout have made the code more legible.

--- a_random_quote_generator-v1 (1)/js/script.js
+++ a_random_quote_generator-v1 (1)/js/script.js
@@ -8,17 +8,16 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array of 13 objects to store varying property/value pairs
+ * `quotes` array of 13 objects with varying property/value pairs
 ***/
 let quotes = [
   {
     quote: "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.",
     source: "Rick Cook",
     citation: "The Wizardry Compiled",
- 
   },
   {
-    quote: "A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don’t touch!",
+    quote: "A son asked his father (a programmer) why the sun rises in the east, and sets in the west. His response? It works, don't touch!",
     source: " u/lugenx",
     year: "2019"
   },
@@ -40,7 +39,6 @@ let quotes = [
     quote: "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
     source: "Bill Gates",
     tags: "Humor, Programming" 
-    
   },
   {
     quote: "Documentation is like sex: when it is good, it is very, very good and when it is bad, it is better than nothing.",
@@ -53,7 +51,7 @@ let quotes = [
     citation: "The C++ Programming Language"
   },
   {
-    quote: "I have not failed. I’ve just found 10,000 ways that won’t work.",
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
     source: "Thomas Edison",
     tags: "Motivation"
   },
@@ -70,37 +68,36 @@ let quotes = [
     tags: "Motivation, Humor" 
   },
   {
-    quote: "“A user interface is like a joke. If you have to explain it, it’s not that good”..",
+    quote: "A user interface is like a joke. If you have to explain it, it's not that good..",
     source: "Martin Leblanc"
   },
   {
-    quote: 'There are only two industries that refer to their customers as "Users"',
+    quote: 'There are only two industries that refer to their customers as "Users."',
     source: "Edward Tufte",
     tags: "Dark Humor" 
   }
 ];
 
 /***
- * `getRandomQuote` function creates a random number to return random quote object for the `quotes` array
+ * `getRandomQuote` function creates a random number which will be used to access different quote objects from the `quotes` array
 ***/
 function getRandomQuote() {
-  let randomNumber = Math.floor(Math.random() * quotes.length);
-  return quotes[randomNumber];
+  let randomQuoteNumber = Math.floor( Math.random() * quotes.length );
+  return quotes[randomQuoteNumber];
 };
+
+/***
+ * `setInterval` method will make the quote to auto update after every 10 sec
+***/
+const quoteInterval = setInterval(printQuote, 10000); 
 
 /***
  * `printQuote` function uses the `getRandomQuote` function to access the random quote objects, and creates html string to be displayed on the browser
 ***/
-/***
- * `setInterval` method will make the quote to auto update after every 10 sec
-***/
-const printQuote = () => {
-  
+function printQuote () {
   let randomQuote = getRandomQuote();
+    //Added `?.` to stop error-> `Uncaught TypeError: Cannot read properties of undefined (reading 'quote')`//
   let quoteString = 
-  /***
- * Added `?.` to stop error-> `Uncaught TypeError: Cannot read properties of undefined (reading 'quote')`
-***/
   `<p class="quote"> ${randomQuote?.['quote']}</p> 
    <p class="source"> ${randomQuote?.['source']}`;
 
@@ -114,23 +111,36 @@ const printQuote = () => {
     quoteString += `<span class="tags"> ${randomQuote?.['tags']} </span></p>`
    };
    document.getElementById('quote-box').innerHTML = quoteString; 
-   return setInterval(printQuote, 10000);  
+  return  quoteString
    };
-   
 
+  /***
+  * `clearInterval` method will make sure the quote only auto updates after every 10 sec is over.
+  ***/ 
+function stopQuoteInterval() {
+  clearInterval(quoteInterval);
+  };
 
-/***
- * `randomColor` function creates a random background color which will be used for each new quote object.
-***/
 /***
  * `setInterval` method will make the background color to auto update after every 10 sec
 ***/
-function randomColor () {
-  const randomNumber = () => Math.floor(Math.random() * 256) +1; 
-  let background = `rgb( ${randomNumber()}, ${randomNumber()}, ${randomNumber()} )`;
-  document.body.style.backgroundColor = background;
-  return setInterval(randomColor, 10000);  
+const colorInterval = setInterval(randomColor, 10000) ;
 
+/***
+ * `randomColor` function uses `randomNumber` function to generate a random number that will be used to create random background colors
+***/
+function randomColor () {
+  const randomColorNumber = () => Math.floor(Math.random() * 256) +1; 
+  let background = `rgb( ${randomColorNumber()}, ${randomColorNumber()}, ${randomColorNumber()} )`;
+  document.body.style.backgroundColor = background;
+return  background
+    };
+
+/***
+ * `stopColorInterval` method will make sure the background color only auto updates after every 10 sec is over.
+***/ 
+function stopColorInterval() {
+ clearInterval(colorInterval);  
 };
 
 /***
